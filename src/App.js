5c2caa4dcda1cdbcfce152d66f77653bd040944d
치치 <chichi@chichiui-MacBookAir.local>
2025-07-27 17:38:48 +0900

    css complete

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const moves = history.map((step, move) => {
   const desc = move ? 'Go to move'+move: ' Go to game start';
   return (
     <li key={move}>
-      <button onClick={() => jumpTo(move)}>{desc}</button>
+      <button className='move-button' onClick={() => jumpTo(move)}>{desc}</button>
     </li>
   )
 })
